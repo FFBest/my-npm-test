51cfde209770d067e56729ae40ec3c9dbe7c5ad4
zf_ccc@foxmail.com <zf_ccc@foxmail.com>
2020-06-28 17:15:58 +0800

    测试 postinstall webpack 打包

--- webpack.commont.js
+++ webpack.commont.js
@@ -6,7 +6,7 @@ module.exports = {
   // entry: "./common.js",
   //entry: entries(),
   entry: Object.assign({
-    commont: `${path.join(__dirname, "../../")}common.js`
+    commont: `${path.join(__dirname, "./")}common.js`
   }, {
     // 用到什么公共lib（例如jquery.js），就把它加进vendor去，目的是将公用库单独提取打包
     // 'vendor': ['jquery']
